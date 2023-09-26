c393fe3b5a9dc61b792948e5aef7da2ce441b5a7
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-09-26 20:12:20 +0530

    single responsibility principle partial code

---
+++ src/main/java/SOLID/S/Employee.java
@@ -0,0 +1,5 @@
+package SOLID.S;
+
+public class Employee {
+
+}
---
+++ src/main/java/SOLID/S/Main.java
@@ -0,0 +1,8 @@
+package SOLID.S;
+
+public class Main {
+    public static void main(String[] args){
+        Employee e = new Employee();
+        System.out.println("Hello there");
+    }
+}
